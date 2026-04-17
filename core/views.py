eeea7fc6d4bd5d5088b8a122f7a460d60377280a
Tu Nombre <tu-email@example.com>
2026-04-16 20:48:47 -0500

    Update home page to display Django welcome image instead of form

--- core/views.py
+++ core/views.py
@@ -1,15 +1,5 @@
 from django.shortcuts import render
-from .forms import ContactForm
 
 
 def home(request):
-    form = ContactForm(request.POST or None)
-    submitted_data = None
-    if request.method == 'POST' and form.is_valid():
-        submitted_data = form.cleaned_data
-        form = ContactForm()
-
-    return render(request, 'core/form.html', {
-        'form': form,
-        'submitted_data': submitted_data,
-    })
+    return render(request, 'core/form.html')
